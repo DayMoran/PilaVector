a3257351acd0ffbcd0c8b86fbfd9f17117c18b51
DayMoran <gabriel.daynor.moran.valencia@outlook.com>
2023-05-15 16:21:23 -0400

    Agregar archivos de proyecto.

---
+++ PilaVector/Pila.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilaVector
+{
+    public class Pila
+    {
+        int max;
+        int[] pila;
+        int top;
+        public Pila(int tam)
+        {
+            max= tam;
+            pila= new int[max];
+            top = -1;
+        }
+        public bool pilaVacia()
+        {
+            if (top<0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool pilaLlena()
+        {
+            if (top==max-1)
+            {
+                return true;
+            }
+            else
+            {
+                return false ;
+            }
+        }
+
+        public void push(int dato)
+        {
+            if (pilaLlena()==false)
+            {
+                top++;
+                pila[top] = dato;
+            }
+            else
+            {
+                Console.WriteLine("La pila Esta LLena...!!!");
+            }
+        }
+        public int pop()
+        {
+            int dato=-1;
+            if (pilaVacia()==false)
+            {
+                dato = pila[top];
+                top--;
+            }
+            else
+            {
+                Console.WriteLine("La Pila esta Vacia...!!!");
+            }
+            return dato;
+        }
+
+        public void muestraPila()
+        {
+            if (pilaVacia()==false)
+            {
+                for (int i = top; i >=0; i--)
+                {
+                    Console.WriteLine("[{0}]", pila[i]);
+                }
+
+            }
+        }
+
+        public void LimpiarPila()
+        {
+            top = -1;
+            Console.WriteLine("La pila ha sido limpiada.");
+        }
+    }
+}
---
+++ PilaVector/Program.cs
@@ -0,0 +1,97 @@
+﻿using PilaVector;
+int op;
+Console.WriteLine("Pila con Vector");
+Console.WriteLine("******************************");
+Console.WriteLine("1.Crear Pila.");
+Console.WriteLine("2.Agregar elemento a la Pila.");
+Console.WriteLine("3.Sacar Elemento de la Pila.");
+Console.WriteLine("4.Mostrar Pila.");
+Console.WriteLine("5.Limpiar Pila");
+Console.WriteLine("6.Salir");
+Console.WriteLine("******************************");
+
+Pila miPila = null;
+
+
+do
+{
+    Console.WriteLine("Ingrese una opcion: ");
+    op = int.Parse(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            Console.WriteLine("Ingrese el tamaño de la pila:");
+            int tam = int.Parse(Console.ReadLine());
+            miPila = new Pila(tam);
+            Console.WriteLine("Se creo la pila.");            
+            break;
+
+        case 2:
+            if (miPila != null)
+            {
+                Console.WriteLine("Ingrese el elemento a agregar:");
+                int elemento = int.Parse(Console.ReadLine());
+                miPila.push(elemento);
+                Console.WriteLine("Elemento agregado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Debe crear una Pila primero.");                
+            }
+            break;
+
+        case 3:
+            if (miPila != null)
+            {
+                int dato = miPila.pop();
+                if (dato != -1)
+                {
+                    Console.WriteLine("Elemento sacado de la pila: {0}", dato);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Debe crear una pila primero.");
+            }
+            break;
+
+        case 4:
+            if (miPila != null)
+            {
+                Console.WriteLine("Contenido de la pila:");
+                miPila.muestraPila();
+            }
+            else
+            {
+                Console.WriteLine("Debe crear una pila primero.");
+            }
+            break;
+        case 5:
+            if (miPila != null)
+            {
+                miPila.LimpiarPila();
+            }
+            else
+            {
+                Console.WriteLine("Debe crear una pila primero.");
+            }
+            break;
+            
+        case 6:
+            Console.WriteLine("Programa terminado.");
+            break;
+
+        default:
+            Console.WriteLine("Opción no válida. Intente nuevamente.");
+            break;
+    }
+
+
+} while(op != 6);
+
+
+
+
+
+
+    
